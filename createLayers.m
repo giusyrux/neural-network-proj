2e22865cdc701f297b8e858183ec5b81dde24f49
Isabella Tieri <i.tieri1998@gmail.com>
2021-09-27 09:08:07 +0000

    Update createLayers.m

--- createLayers.m
+++ createLayers.m
@@ -1,20 +1,22 @@
-function [layers]=createLayers(params, inputSize, outputSize)
+function layers = createLayers(params,inputSize,outputSize)
+%params: parametri dati dall'utente utili alla costruzione della rete
+%inputSize: numero di variabili in ingresso (x1,...,xd)
+%outputSize: numero nodi dello strato di output pari al numero di classi
 
-%params = parametri dati dall'utente utili alla costruzione della rete
-%inputSize = numero di variabili in ingresso (x1,...,xd)
-%outputSize = numero nodi dello strato di output pari al numero di classi
-
-    neurons = params.neurons; %array di lunghezza = n strati e ogni cella contiene numero di neuroni
+    neurons = params.neurons;
     nLayers = params.nLayers;
     layers = repmat(struct('W', zeros(0)), 1, nLayers); %uno strato sarà una struct con i suoi campi
 
     %inizializzo il parametro size per ogni strato per determinare le
     %dimensioni delle matrici dei pesi e del gradiente
     if isempty(neurons)
+        
         layers(1).size = [inputSize,outputSize];
     else 
+        
         layers(1).size = [inputSize, neurons(1)];
         for i = 2:nLayers-1	
+            
             layers(i).size = [neurons(i-1), neurons(i)];
         end	
         layers(nLayers).size = [neurons(end), outputSize];
@@ -23,6 +25,7 @@ function [layers]=createLayers(params, inputSize, outputSize)
 
     %imposto i parametri per ogni strato
     for i=1:nLayers 
+        
         layers(i).act = params.act(i); %funzione di attivazione per ogni strato
         layers(i).W = randn(layers(i).size(2),layers(i).size(1)); %matrice pesi
         layers(i).B = zeros(layers(i).size(2),1); %vettore di bias
