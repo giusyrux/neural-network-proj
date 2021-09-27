b226608ed0803cbcd66157aee962d15b0027d981
Isabella Tieri <i.tieri1998@gmail.com>
2021-09-27 09:02:08 +0000

    Update sumOfSquares.m

--- sumOfSquares.m
+++ sumOfSquares.m
@@ -1,3 +1,7 @@
-function e=sumOfSquares(y,t)
-    e=(1/2)*sum(sum((y-t).^2));
+function err = sumOfSquares(y,t)
+%calcolo dell'errore tramite sum-of-squares
+%y: output della rete
+%t: label del DS
+
+    err = (1/2)*sum(sum((y-t).^2));
 end
