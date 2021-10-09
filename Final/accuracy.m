3671d66833d7f8536ee4efa958053092dde08554
Isabella Tieri <i.tieri1998@gmail.com>
2021-10-09 09:23:57 +0000

    Carica un nuovo file

---
+++ Final/accuracy.m
@@ -0,0 +1,21 @@
+function score = accuracy(y,t)
+
+    scores = zeros(1,size(y,2)); %inizializzo gli score
+    
+    %fisso una soglia di valutazione
+    y(y>0.6)=1;
+    y(y<=0.6)=0;
+    
+    for img=1:size(y,2)
+        for class=1:size(y,1)
+            
+            if y(class,img) == t(class,img)
+                scores(img) = 1;
+            else
+                scores(img) = 0;
+            end
+        end
+    end
+    
+    score = mean(scores); %faccio la media degli score
+end
